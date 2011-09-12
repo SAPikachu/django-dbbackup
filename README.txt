f890c0036b60d7778216c0c6409c64b46f18811b
Michael Shepanski <mjs7231@gmail.com>
2011-09-12 00:02:29 -0400

    Fix S3_DOMAIN setting

--- README.txt
+++ README.txt
@@ -80,7 +80,8 @@ DBBACKUP_S3_DIRECTORY (optional)
 DBBACKUP_S3_DOMAIN (optional)
     Optionally specify the Amazon domain to use when transferring the
     generated backup files. For example, this can be set to
-    s3-eu-west-1.amazonaws.com. By default, this is 's3.amazonaws.com'.
+    http://s3-eu-west-1.amazonaws.com/. By default, this is
+    'https://s3.amazonaws.com/'.
 
 
 
--- dbbackup/storage/s3_storage.py
+++ dbbackup/storage/s3_storage.py
@@ -5,6 +5,7 @@ import os, tempfile, sys
 from .base import BaseStorage, StorageError
 from django.conf import settings
 from simples3.streaming import StreamingS3Bucket
+from simples3.utils import aws_urlquote
 
 
 ################################
@@ -16,15 +17,16 @@ class Storage(BaseStorage):
     S3_BUCKET = getattr(settings, 'DBBACKUP_S3_BUCKET', None)
     S3_ACCESS_KEY = getattr(settings, 'DBBACKUP_S3_ACCESS_KEY', None)
     S3_SECRET_KEY = getattr(settings, 'DBBACKUP_S3_SECRET_KEY', None)
-    S3_DOMAIN = getattr(settings, 'DBBACKUP_S3_DOMAIN', 's3.amazonaws.com')
+    S3_DOMAIN = getattr(settings, 'DBBACKUP_S3_DOMAIN', 'https://s3.amazonaws.com/')
     S3_DIRECTORY =  getattr(settings, 'DBBACKUP_S3_DIRECTORY', "django-dbbackups/")
     S3_DIRECTORY = '%s/' % S3_DIRECTORY.strip('/')
 
     def __init__(self, server_name=None):
         self._check_filesystem_errors()
         self.name = 'AmazonS3'
+        self.baseurl = self.S3_DOMAIN + aws_urlquote(self.S3_BUCKET)
         self.bucket = StreamingS3Bucket(self.S3_BUCKET, self.S3_ACCESS_KEY,
-            self.S3_SECRET_KEY, amazon_s3_domain=self.S3_DOMAIN)
+            self.S3_SECRET_KEY, base_url=self.baseurl)
         BaseStorage.__init__(self)
 
     def _check_filesystem_errors(self):

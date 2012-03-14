9c5682bfb7f355f31dc4c38b66cbac1e8fc82141
Michael Shepanski <mjs7231@gmail.com>
2012-03-13 22:56:59 -0400

    Up version for pyPI

--- setup.py
+++ setup.py
@@ -21,11 +21,12 @@ for dirpath, dirnames, filenames in os.walk(package_dir):
 
 
 setup(name='django-dbbackup',
-    version='1.0',
-    description='Django Dropbox Backup',
+    version='1.2',
+    description='Management commands to help backup and restore a project database to AmazonS3, Dropbox or local disk.',
     long_description=read('README.txt'),
     author='Michael Shepanski',
+    author_email='mjs7231@gmail.com',
     license='BSD',
     url='http://bitbucket.org/mjs7231/django-dbbackup',
-    keywords = ['django','dropbox','database','backup'],
+    keywords = ['django','dropbox','database','backup','amazon','s3'],
     packages=packages)

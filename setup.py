75cd11a60394cc9b40c213bbe8a6e026d386ed14
Michael Shepanski <mjs7231@gmail.com>
2012-04-04 22:32:44 -0400

    Up version to 1.62 for pip deploy

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ for dirpath, dirnames, filenames in os.walk(package_dir):
 
 
 setup(name='django-dbbackup',
-    version='1.61',
+    version='1.62',
     description='Management commands to help backup and restore a project database to AmazonS3, Dropbox or local disk.',
     long_description=read('README.txt'),
     author='Michael Shepanski',
